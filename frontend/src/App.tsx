8371535dde65764848da15cf70b0baf37b801142
Kanhayadav <kanhayadav1610@gmail.com>
2026-05-19 20:37:35 +0530

    added public route fucntion to the / path as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
         <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>}></Route>
 
         <Route path="/login" element={<PublicRoute><Login /></PublicRoute>}></Route >
